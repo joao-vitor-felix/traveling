d9cb96d960cda2ebc31d54f0ba474abc2d94b2d7
João Vitor <joaovitorfelixcontato@gmail.com>
2023-09-20 10:45:01 -0300

    feat: added Input, CurrencyInput and DatePicker

---
+++ src/components/DatePicker/DatePicker.tsx
@@ -0,0 +1,40 @@
+import { forwardRef } from "react";
+import { twMerge } from "tailwind-merge";
+import _DatePicker, {
+  ReactDatePickerProps,
+  registerLocale
+} from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import ptBR from "date-fns/locale/pt-BR";
+
+registerLocale("pt-BR", ptBR);
+
+interface InputProps extends ReactDatePickerProps {
+  error?: boolean;
+  errorMessage?: string;
+}
+
+function DatePicker({ className, error, errorMessage, ...props }: InputProps) {
+  const datePickerClassName = twMerge(
+    "rounded-lg border border-gray-300 bg-white p-2 text-sm font-normal text-primaryLighter placeholder-black placeholder-opacity-20 outline-none transition-all focus:ring-1 focus:ring-primary",
+    error ? "border-red-500" : "",
+    className
+  );
+
+  return (
+    <div className="flex w-full flex-col">
+      <_DatePicker
+        locale="pt-BR"
+        wrapperClassName="w-full"
+        className={datePickerClassName}
+        enableTabLoop={false}
+        {...props}
+      />
+      {error && errorMessage && (
+        <div className="text-red-500 mt-1 text-xs">{errorMessage}</div>
+      )}
+    </div>
+  );
+}
+
+export default forwardRef(DatePicker);
