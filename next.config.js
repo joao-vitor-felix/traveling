b096dff70abd57ae49af00f855b28bf152fb986b
João Vitor <joaovitorfelixcontato@gmail.com>
2023-09-21 12:05:54 -0300

    feat: added TripItem

--- next.config.js
+++ next.config.js
@@ -6,6 +6,11 @@ const nextConfig = {
         protocol: "https",
         hostname: "lh3.googleusercontent.com",
         port: ""
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        port: ""
       }
     ]
   }
---
+++ src/components/TripItem/TripItem.tsx
@@ -0,0 +1,38 @@
+import { Trip } from "@prisma/client";
+import Image from "next/image";
+import React from "react";
+import ReactCountryFlag from "react-country-flag";
+
+interface TripItemProps {
+  trip: Trip;
+}
+
+const TripItem = ({ trip }: TripItemProps) => {
+  return (
+    <div className="flex flex-col">
+      <div className="relative h-[280px] w-[280px] cursor-pointer">
+        <Image
+          src={trip.coverImage}
+          className="object-cover rounded-lg shadow-md"
+          fill
+          alt={`Imagem da viagem ${trip.name}`}
+        />
+      </div>
+
+      <h3 className="text-primary font-medium text-md mt-2">{trip.name}</h3>
+      <div className="flex items-center gap-1 my-1">
+        <ReactCountryFlag countryCode={trip.countryCode} svg />
+        <p className="text-sm text-gray">{trip.location}</p>
+      </div>
+
+      <p className="text-sm text-gray-900 ">
+        <span className="text-primary font-medium mr-1">
+          R${trip.pricePerDay.toString()}
+        </span>
+        por dia
+      </p>
+    </div>
+  );
+};
+
+export default TripItem;
