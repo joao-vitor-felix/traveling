9fbcd6a7d1a158d0b8de4e421e8716df972c32f0
João Vitor <joaovitorfelixcontato@gmail.com>
2023-09-24 02:46:00 -0300

    feat: added Link to TripItem

--- src/components/TripItem/TripItem.tsx
+++ src/components/TripItem/TripItem.tsx
@@ -1,5 +1,6 @@
 import { Trip } from "@prisma/client";
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 import ReactCountryFlag from "react-country-flag";
 
@@ -10,14 +11,14 @@ interface TripItemProps {
 const TripItem = ({ trip }: TripItemProps) => {
   return (
     <div className="flex flex-col">
-      <div className="relative h-[280px] w-[280px] cursor-pointer">
+      <Link href={`/trips/${trip.id}`} className="relative h-[280px] w-[280px]">
         <Image
           src={trip.coverImage}
           className="object-cover rounded-lg shadow-md"
           fill
           alt={`Imagem da viagem ${trip.name}`}
         />
-      </div>
+      </Link>
 
       <h3 className="text-primary font-medium text-md mt-2">{trip.name}</h3>
       <div className="flex items-center gap-1 my-1">
@@ -25,7 +26,7 @@ const TripItem = ({ trip }: TripItemProps) => {
         <p className="text-sm text-gray">{trip.location}</p>
       </div>
 
-      <p className="text-sm text-gray-900 ">
+      <p className="text-sm text-gray-900">
         <span className="text-primary font-medium mr-1">
           R${trip.pricePerDay.toString()}
         </span>
