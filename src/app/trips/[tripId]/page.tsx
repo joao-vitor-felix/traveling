301ed316e93bae16721099b7031ba1d933dd3c03
João Vitor <joaovitorfelixcontato@gmail.com>
2023-09-24 02:52:50 -0300

    feat: added TripDetails page

---
+++ src/app/trips/[tripId]/page.tsx
@@ -0,0 +1,60 @@
+import { prisma } from "@/lib/prisma";
+import TripHeader from "./components/TripHeader";
+import TripReservation from "./components/TripReservation";
+import TripDescription from "./components/TripDescription";
+import TripHighlights from "./components/TripHighlights";
+import TripLocation from "./components/TripLocation";
+import { FC } from "react";
+
+const getTripDetails = async (tripId: string) => {
+  let trip;
+  try {
+    trip = await prisma.trip.findUnique({
+      where: {
+        id: tripId
+      }
+    });
+  } finally {
+    await prisma.$disconnect();
+  }
+
+  return trip;
+};
+
+export async function generateMetadata({
+  params
+}: {
+  params: { tripId: string };
+}) {
+  const trip = await getTripDetails(params.tripId);
+
+  if (!trip) return null;
+
+  return {
+    title: `${trip.name} | Traveling`,
+    description: "Trip Details"
+  };
+}
+
+const TripDetails: FC<{ params: { tripId: string } }> = async ({ params }) => {
+  const trip = await getTripDetails(params.tripId);
+
+  if (!trip) return null;
+
+  return (
+    <div className="container mx-auto">
+      <TripHeader trip={trip} />
+      <TripReservation trip={trip} />
+      <TripDescription description={trip.description} />
+      <TripHighlights highlights={trip.highlights} />
+      <TripLocation
+        locationDescription={trip.locationDescription}
+        location={trip.location}
+        latitude={trip.latitude}
+        longitude={trip.longitude}
+      />
+    </div>
+  );
+};
+
+export default TripDetails;
