cdb8555f47845360309f93bb1535a082c6f419fd
João Vitor <joaovitorfelixcontato@gmail.com>
2023-09-26 20:29:23 -0300

    feat: added semantic tag into Footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,13 +2,13 @@ import { BiSolidPlaneTakeOff } from "react-icons/bi";
 
 const Footer = () => {
   return (
-    <div className="container mx-auto flex flex-col gap-1 items-center p-5">
+    <footer className="container mx-auto flex flex-col gap-1 items-center p-5">
       <div className="flex items-center gap-1">
         <BiSolidPlaneTakeOff size={28} />
         <h1 className="text-gray-900 text-lg">Traveling</h1>
       </div>
       <p className="text-gray-900 text-sm">Todos os direitos reservados.</p>
-    </div>
+    </footer>
   );
 };
 
