a2022bf9b9385210c2bba0ea3aada0e6a1529543
João Vitor <joaovitorfelixcontato@gmail.com>
2023-09-28 11:56:04 -0300

    feat: added danger variant

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,13 +2,15 @@ import { ComponentPropsWithoutRef } from "react";
 import { twMerge } from "tailwind-merge";
 
 interface ButtonProps extends ComponentPropsWithoutRef<"button"> {
-  variant?: "primary" | "outlined";
+  variant?: "primary" | "outlined" | "danger";
 }
 
 function Button({ className, variant = "primary", ...props }: ButtonProps) {
   const variantClasses = {
     primary: "bg-gray-900 text-white hover:bg-gray-700",
-    outlined: "bg-transparent border-2 border-primary text-gray-900"
+    outlined: "bg-transparent border-2 border-primary text-gray-900",
+    danger:
+      "text-red-500 border-red-500 border hover:bg-red-600 bg-transparent hover:text-white"
   };
 
   const _className = twMerge(
