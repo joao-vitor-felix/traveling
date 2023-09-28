0290121779e26299a0f9c4ae81984ba689df0ed1
João Vitor <joaovitorfelixcontato@gmail.com>
2023-09-28 10:23:24 -0300

    feat: added callback into next auth in order to get the user id

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -12,7 +12,18 @@ export const authOptions: AuthOptions = {
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!
     })
-  ]
+  ],
+  callbacks: {
+    async session({ session, user }) {
+      session.user = { ...session.user, id: user.id } as {
+        id: string;
+        name: string;
+        email: string;
+      };
+
+      return session;
+    }
+  }
 };
 
 const handler = NextAuth(authOptions);
