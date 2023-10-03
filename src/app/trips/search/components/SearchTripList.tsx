b9f44bf13641522abacc18daa1cf036809ea171a
João Vitor <joaovitorfelixcontato@gmail.com>
2023-10-03 11:46:04 -0300

    feat: added desktop style to SearchTripList

--- src/app/trips/search/components/SearchTripList.tsx
+++ src/app/trips/search/components/SearchTripList.tsx
@@ -30,16 +30,16 @@ const SearchTripList = () => {
 
   return (
     <main className="container mx-auto flex flex-col items-center p-5 gap-2">
-      <h1 className="text-primary font-semibold text-xl">
+      <h1 className="text-primary font-semibold text-xl lg:text-[2.5rem] lg:self-start">
         Viagens Encontradas
       </h1>
-      <h2 className="text-gray-900 font-medium mb-5 text-center">
+      <h2 className="text-gray-900 font-medium mb-5 text-center lg:mt-6 lg:self-start">
         {trips.length > 0
           ? "Listamos as melhores viagens pra você!"
           : "Não encontramos viagens nas condições selecionadas! =("}
       </h2>
 
-      <div className="flex flex-col gap-4">
+      <div className="flex flex-col gap-4 lg:flex-row lg:flex-wrap lg:gap-8 lg:items-center lg:justify-center">
         {trips?.map(trip => <TripItem key={trip.id} trip={trip} />)}
       </div>
     </main>
