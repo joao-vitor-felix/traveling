d387b88793c75a6539bc089dda16e15cda8a6035
João Vitor <joaovitorfelixcontato@gmail.com>
2023-10-03 15:52:34 -0300

    feat: added desktop style to Navigation

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -48,7 +48,7 @@ const Navigation = () => {
   }, [isMenuOpen]);
 
   return (
-    <header className="container mx-auto p-5 py-0 h-[93px] flex justify-between items-center">
+    <header className="container mx-auto p-5 py-0 h-[93px] flex justify-between items-center lg:border-b lg:border-primaryLighter">
       <Link href="/" className="flex items-center gap-1">
         <BiSolidPlaneTakeOff size={28} />
         <h1 className="text-gray-900 text-xl">Traveling</h1>
