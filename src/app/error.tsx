9e7c601a31ccaea8cc7a31faa79bb46683447cd2
joao-vitor-felix <joaovitorfelixcontato@gmail.com>
2024-01-31 15:06:18 -0300

    feat: add go to home button on ErrorPage

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -4,6 +4,7 @@ import Button from "@/components/Button/Button";
 import Image from "next/image";
 import { FC } from "react";
 import * as Sentry from "@sentry/nextjs";
+import { useRouter } from "next/navigation";
 
 type ErrorProps = {
   error: Error;
@@ -13,15 +14,26 @@ type ErrorProps = {
 const ErrorPage: FC<ErrorProps> = ({ error, reset }) => {
   Sentry.captureException(error);
 
+  const router = useRouter();
+
+  const handleHomeClick = () => {
+    router.push("/");
+  };
+
   return (
     <main className="flex flex-col items-center justify-center pt-12 gap-4">
       <h1 className="text-base text-center font-semibold text-primary lg:text-3xl">
         Algo deu errado! Tente novamente mais tarde.
       </h1>
       <Image src="/500-error.svg" alt="404" width={400} height={400} />
-      <Button onClick={() => reset()} className="w-60">
-        Retornar
-      </Button>
+      <div className="flex flex-col gap-2 lg:flex-row ">
+        <Button onClick={() => reset()} className="w-60">
+          Tentar novamente
+        </Button>
+        <Button onClick={handleHomeClick} className="w-60">
+          Ir para a página inicial
+        </Button>
+      </div>
     </main>
   );
 };
